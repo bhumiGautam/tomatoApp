e8808d8c8497187c450b29e515ba2b4cec598237
bhumiGautam <138861451+bhumiGautam@users.noreply.github.com>
2026-02-28 22:48:04 +0530

    Update StoreContext.jsx

--- Frontend/src/Context/StoreContext.jsx
+++ Frontend/src/Context/StoreContext.jsx
@@ -6,7 +6,7 @@ export const StoreContext = createContext(null);
 
 const StoreContextProvider = (props) => {
 
-    const url = "http://localhost:4000";
+    const url = "https://tomatoapp-eyat.onrender.com";
 
     const [cartItems, setCartItems] = useState({});
     const [foodList, setFoodList] = useState([]);
