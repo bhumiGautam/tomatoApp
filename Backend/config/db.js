f75a935b1058db0e30426e82f0c12dad97ee9262
bhumiGautam <gautamkumarpandey149@gmail.com>
2026-02-28 22:23:16 +0530

    db config changes

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -2,7 +2,6 @@ import mongoose from "mongoose";
 
 
  export const connectDB= async ()=>{
-    // mongodb+srv://bhumigautam25:7759887117@cluster0.r5smjxh.mongodb.net
-    await mongoose.connect('mongodb+srv://gautamkumarpandey149_db_user:IwdxEUyJpQ5f8mWV@newconnection.cupauuw.mongodb.net/?appName=NewConnection/food').then(()=>console.log("DB Connected"));
+   
+    await mongoose.connect(process.env.MONGO_URI).then(()=>console.log("DB Connected"));
 }
-// mongodb://localhost:27017/food-del
